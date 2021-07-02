5576bd65b992f9fbedf74598d7fcaab888fe87c3
amr205 <alejandromedinareyesmx@gmail.com>
2021-07-02 15:33:03 -0600

    aplicacion lista

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const express = require("express")
-
 const app = express()
-const port = (process.env.PORT || '4300');
+const port = (process.env.PORT || '4300')
+
 
 app.get("/",(req,res)=>{
-    res.send('Hello World!')
+    res.send("Hola mi nombre es alex")
 })
 
 app.listen(port)
