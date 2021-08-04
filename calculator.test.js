e92fb7a6349a3a27fba6a24a35f702153d989cec
amr205 <alejandromedinareyesmx@gmail.com>
2021-08-04 16:55:08 -0600

    fail my test

--- calculator.test.js
+++ calculator.test.js
@@ -1,7 +1,7 @@
 sum = require("./calculator")
 
 test('(local) verificar suma de dos números positivos', ()=>{
-    expect(sum(5,4)).not.toBe(10)
+    expect(sum(5,4)).toBe(9)
 })
 
 test('(prod) verificar suma de dos números positivos, caso 2', ()=>{
